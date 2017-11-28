0740d32ace10b99dd2e2804d3d819b25a43958e1
J3ffcon1 <Jrlone1990@gmail.com>
2017-11-27 20:52:38 -0800

    troubleshooting problems solved, fixed repo link

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -10,9 +10,11 @@
     //       Remember that the callback function we'll want to call relies on repos.all
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
-      $.get(`https://api.github.com/?access_token=${token}`) //string interpolation
+
+      $.ajax(`https://api.github.com/user/repos?access_token=${token}`) //string interpolation
       .then(
         results => {
+          console.log('results', results);
           repos.all = results;
           callback();
         }
