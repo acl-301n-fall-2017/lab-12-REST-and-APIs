8d950c3c3466f18a2fd1e703f157017880559a7e
nhoszko@gmail.com <nhoszko@gmail.com>
2017-11-28 18:43:11 -0800

    Completed

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -10,7 +10,19 @@
     //       Remember that the callback function we'll want to call relies on repos.all
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
-
+    $.ajax
+    ({
+      type: "GET",
+      url: `https://api.github.com/user/repos?access_token=${token}`,
+      })
+      .then( 
+        results => {
+          repos.all = results;
+          console.log('Repos returned', results);
+          callback();
+        },
+        error => console.log('DANGER!')
+      )
   };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
