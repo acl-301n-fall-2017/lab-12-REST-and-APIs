b52530b0727ce028273c9efa1c50e10a1b5c22d9
Mario Quintana <pyrros2000@gmail.com>
2018-05-30 19:36:22 -0700

    added new scripts and started working on the repo js

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -11,6 +11,15 @@
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
 
+    $.ajax({
+      dataType: "json",
+      url:'https://api.github.com/users/Quintam26/repos',
+      data: "data",
+      success: function (data) {
+      // const info = data;
+      repos.all.push(data)  
+      }
+    });
   };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
