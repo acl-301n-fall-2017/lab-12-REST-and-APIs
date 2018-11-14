e7f3785f36e400cd585be158797dfe0d8a200487
Kate Dameron <katerj33@gmail.com>
2018-11-13 19:30:09 -0800

    set up api, pushed it to the array and set the callback

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -10,7 +10,8 @@
     //       Remember that the callback function we'll want to call relies on repos.all
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
-
+    repos.all = ($.getJSON('https://api.github.com/users/katedam/repos'));
+    
   };
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
